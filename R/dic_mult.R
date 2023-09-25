e7b0e99e4cde35a7dcb0b04f8db48eb135bfbd10
jmarmin <jomarminaya@gmail.com>
2023-09-25 13:29:03 +0200

    checking R-package

--- R/dic_mult.R
+++ R/dic_mult.R
@@ -2,8 +2,8 @@
 #'
 #' `dmnorm` computes the multivariate joint density Gaussian distribution
 #'
-#' @param y a vector (1 x d) or matrix (n x d) of multivariate observations  
-#' @param mu a vector (1 x d) or matrix (n x d) of multivariate observations 
+#' @param y a vector (1 x d) or matrix (n x d) of multivariate observations
+#' @param mu a vector (1 x d) or matrix (n x d) of multivariate observations
 #' @param Sigma variance-covariance matrix (n x d)
 #' @param log1 TRUE if logaritmith transformation is used. Default is FALSE.
 #'
@@ -11,14 +11,15 @@
 #' @export
 #' @import dplyr
 #' @import Matrix
+#' @import stringr
 #' @author Joaquín Martínez-Minaya <\email{jmarminaya@@gmail.com}>
 dmnorm <- function(y, mu, Sigma, log1 = FALSE){
   k <- ncol(Sigma)
   y <- t(y)
   mu <- t(mu)
-  dmn <- exp((-1/2)*diag(t(y-mu)%*%solve(Sigma)%*%(y-mu)))/sqrt(((2*pi)^k)*det(Sigma))  
+  dmn <- exp((-1/2)*diag(t(y-mu)%*%solve(Sigma)%*%(y-mu)))/sqrt(((2*pi)^k)*det(Sigma))
   if(log1 == TRUE){
-    log(dmn)  
+    log(dmn)
   }else{
     dmn
   }
@@ -30,7 +31,7 @@ dmnorm <- function(y, mu, Sigma, log1 = FALSE){
 #'
 #' `extract_lp_sigma` Extracts posterior distribution of the linear predictor a of the covariance matrix from an object obtained from inla.posterior.sample
 #'
-#' @param xx a resulting object of applying the function `inla.posterior.sample` 
+#' @param xx a resulting object of applying the function `inla.posterior.sample`
 #'
 #' @return A list with two objects: 1. Linear predictor substracting id.z. 2. Posterior Covariance matrix
 #' @export
@@ -42,22 +43,22 @@ extract_lp_sigma <- function(xx){
     stringr::str_starts("Precision for the Gaussian observations") %>%
     xx$hyperpar[.]
   len <- length(hyper_like)
-  
+
   hyper_cov <- xx$hyperpar %>% names(.) %>%
     stringr::str_starts("Precision for id.z") %>%
     xx$hyperpar[.]
-  
-  Sigma_post <- diag(1/hyper_like) + 
+
+  Sigma_post <- diag(1/hyper_like) +
     matrix(1/hyper_cov, ncol = len, nrow = len)
-  
+
   #Predictor
   predictor <- xx$latent %>% rownames(.) %>%
-    stringr::str_starts("APredictor") %>% xx$latent[.] 
-  reffect <- xx$latent %>% rownames(.) %>% 
+    stringr::str_starts("APredictor") %>% xx$latent[.]
+  reffect <- xx$latent %>% rownames(.) %>%
     stringr::str_starts("id.z") %>% xx$latent[.] %>% rep(., len)
-  
+
   pred1 <- (predictor - reffect) %>% matrix(., ncol = len, byrow = FALSE)
-  list(predictor = pred1, 
+  list(predictor = pred1,
        Sigma = Sigma_post)
 }
 
@@ -77,32 +78,32 @@ dic.mult <- function(inf, y){
   #Expected deviance
   e.dev <- mclapply(inf,
                     function(x){
-                      a <- -2*dmnorm(mu = x$predictor, 
-                                     Sigma = x$Sigma, 
-                                     y = y, 
+                      a <- -2*dmnorm(mu = x$predictor,
+                                     Sigma = x$Sigma,
+                                     y = y,
                                      log1 = TRUE) %>% sum(.)}) %>%
     unlist(.) %>%
     mean(.)
-  
+
   #Deviance of the expected value
   pred_mean <- inf %>%
     lapply(., `[[`, "predictor") %>%
     Reduce("+", .)/nsim
-  
+
   Sigma_post <- inf %>%
     lapply(., `[[`, "Sigma") %>%
     Reduce("+", .)/nsim
-  
-  dev.e <- sum(-2*dmnorm(y = as.matrix(y), 
-                         mu = pred_mean, 
-                         Sigma = Sigma_post, 
+
+  dev.e <- sum(-2*dmnorm(y = as.matrix(y),
+                         mu = pred_mean,
+                         Sigma = Sigma_post,
                          log1 = TRUE))
-  
+
   p.eff <- e.dev - dev.e
   dic1 <- p.eff + e.dev
   data.frame(e.dev = e.dev,
              dev.e = dev.e,
-             p.eff = p.eff, 
+             p.eff = p.eff,
              dic   = dic1)
 }
 
@@ -119,28 +120,28 @@ dic.mult <- function(inf, y){
 #' @author Joaquín Martínez-Minaya <\email{jmarminaya@@gmail.com}>
 waic.mult <- function(inf, y){
   eval.p <- sapply(inf, function(x){
-    dmnorm(mu = x$predictor, 
-           Sigma =x$Sigma, 
-           y = y, 
+    dmnorm(mu = x$predictor,
+           Sigma =x$Sigma,
+           y = y,
            log1 = TRUE)})
-  
-  p.eff.waic <- apply(eval.p, 1, var) %>% 
+
+  p.eff.waic <- apply(eval.p, 1, var) %>%
     sum(.)
-  
-  
+
+
   eval.p2 <- sapply(inf, function(x){
-    dmnorm(mu = x$predictor, 
-           Sigma =x$Sigma, 
-           y = y, 
+    dmnorm(mu = x$predictor,
+           Sigma =x$Sigma,
+           y = y,
            log1 = FALSE)})
-  
-  lppd <- eval.p2 %>% 
+
+  lppd <- eval.p2 %>%
     apply(., 1, mean) %>%
-    log(.) %>% 
+    log(.) %>%
     sum(.)
-  
+
   WAIC.1 <- -2*(lppd - p.eff.waic)
-  
+
   data.frame(p.eff.waic = p.eff.waic,
              waic       = WAIC.1)
 }
--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 
-# INLAcomp
+INLAcomp
+------------
 
-The goal of **INLAcomp** is to analyze compositional data with Logistic Normal distribution regression with Dirichlet residuals using the integrated nested Laplace approximation
-via the [R-INLA package](https://www.r-inla.org/).
+The goal of **INLAcomp** is to analyze compositional data with Logistic Normal distribution regression with Dirichlet residuals (LNDM) using the integrated nested Laplace approximation
+via the [R-INLA package](https://www.r-inla.org/). Method is in [Martinez-Minaya and Rue, 2023](https://arxiv.org/abs/2308.13928).
+
+- For a **tutorial about how to fit a simple LNDM model using R-INLA**, click [here](https://github.com/jmartinez-minaya/INLAcomp/blob/main/vignettes/Dirichlet-CoDa.Rmd) or [here](https://github.com/hrue/r-inla/commit/3577c1b030a8460ff7194893bd97e57a62a1399d)
+
+- The code for the paper is available in the [vignette](https://github.com/jmartinez-minaya/INLAcomp/blob/main/vignettes/my-vignette.Rmd). The generated html is available [here](https://jmartinez-minaya.github.io/supplementary/INLAComp/my-vignette.html)
 
 
-UNDER CONSTRUCTION
+
+
+
+Installation
+------------
+
+It is not yet in CRAN, but you can install the latest bugfix release of **INLAcomp** from [github](https://github.com/jmartinez-minaya/INLAcomp) with:
+
+
